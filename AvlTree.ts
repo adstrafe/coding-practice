87580aa8558f06a0266e7788c90c1051c7455c83
ADStrafe <vojtech.lahuta@gmail.com>
2024-09-22 21:06:28 +0200

    Implement rotations

--- AvlTree.ts
+++ AvlTree.ts
@@ -3,7 +3,8 @@ class AvlNode {
 		public value: number,
 		public left: AvlNode | undefined = undefined,
 		public right: AvlNode | undefined = undefined,
-		public height: number = 1
+		public height: number = 0,
+		public balance: number = 0
 	) {}
 }
 
@@ -23,7 +24,7 @@ class AvlTree {
 
 		if (node.value === value) {
 			console.log('Duplicate value: ', value);
-			return;
+			return node;
 		}
 
 		if (value < node.value) {
@@ -34,14 +35,49 @@ class AvlTree {
 		}
 
 		node.height = 1 + Math.max(this.getHeight(node.left), this.getHeight(node.right));
-		this.balance();
+		node.balance = this.getHeight(node.left) - this.getHeight(node.right);
+		return this.balance(node);
 	}
 
 	private getHeight(node: AvlNode | undefined) {
 		return node === undefined ? 0 : node.height;
 	}
 
-	private balance() {
+	private balance(node: AvlNode) {
 		// TODO
+	}
+
+	private rotateRight(y: AvlNode) {
+		const x = y.left!;
+		const subTree = x.right;
+
+		x.right = y;
+		y.left = subTree;
+
+		x.height = 1 + Math.max(this.getHeight(x.left), this.getHeight(x.right));
+		y.height = 1 + Math.max(this.getHeight(y.left), this.getHeight(y.right));
+
+		x.balance = this.getHeight(x.left) - this.getHeight(x.right);
+		y.balance = this.getHeight(y.left) - this.getHeight(y.right);
+
+		// return the new root of the subtree
+		return x;
+	}
+
+	private rotateLeft(y: AvlNode) {
+		const x = y.right!;
+		const subTree = x.left;
+
+		x.left = y;
+		y.right = subTree;
+
+		y.height = 1 + Math.max(this.getHeight(y.left), this.getHeight(y.right));
+		x.height = 1 + Math.max(this.getHeight(x.left), this.getHeight(x.right));
+		
+		y.balance = this.getHeight(y.left) - this.getHeight(y.right);
+		x.balance = this.getHeight(x.left) - this.getHeight(x.right);
+
+		// return the new root of the subtree
+		return x;
 	}
 }
